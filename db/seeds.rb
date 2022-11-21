f1d5f7b9e022d106254d0c568743efad8bc4a584
Nicholas Drömert <campbell.cnp@gmail.com>
2022-11-21 17:22:05 +0100

    Seed with local image that uploads to cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,25 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# puts "Cleaning up database..."
-# Trips.destroy
+puts "Cleaning up database..."
+Trip.delete_all
+User.delete_all
+
 puts "Creating trips"
+test_user = User.create(
+  email: "test.email@google.com",
+  password: "123456"
+)
 
 test_trip = Trip.create!(
   name: "A test trip",
   price: 70,
   description: "Why a test trip you ask? MSC is testing out the very latest in rocket technology - save 15% on your
-                booking by flying with us today"
+                booking by flying with us today",
+  user: test_user
 )
 
 path = File.join(Rails.root, 'app/assets/images/seed_image.jpg')
-Trip.find(1).photos.attach(io: File.open(path), filename: "seed_image.jpg", content_type: "image/jpeg")
+test_trip.photos.attach(io: File.open(path), filename: "seed_image.jpg", content_type: "image/jpeg")
+
+puts "Finished!"
