668c049a4f04c6dd1bb6e2de7c94fcbfd4a49252
Nicholas Drömert <campbell.cnp@gmail.com>
2022-11-21 17:06:20 +0100

    Work in progress

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,5 @@
 class Trip < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
   has_many :bookings
   has_many_attached :photos
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# puts "Cleaning up database..."
+# Trips.destroy
+puts "Creating trips"
+
+test_trip = Trip.create!(
+  name: "A test trip",
+  price: 70,
+  description: "Why a test trip you ask? MSC is testing out the very latest in rocket technology - save 15% on your
+                booking by flying with us today"
+)
+
+path = File.join(Rails.root, 'app/assets/images/seed_image.jpg')
+Trip.find(1).photos.attach(io: File.open(path), filename: "seed_image.jpg", content_type: "image/jpeg")
