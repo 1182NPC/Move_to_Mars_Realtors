299b7e9aa40e84b794a84338ec9dc3ec7f142851
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2022-11-21 15:12:37 +0100

    yep

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,6 +1,7 @@
 class TripsController < ApplicationController
 
   def new
+    @list = List.new
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :trips, only: [:index]
+  resources :trips, only: [:index, :new]
   resources :bookings, only: [:index]
 end
