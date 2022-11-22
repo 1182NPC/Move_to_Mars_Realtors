c24a08388c446a63fcbdfa10e7af05adea0e8208
Nicholas Drömert <campbell.cnp@gmail.com>
2022-11-22 13:00:36 +0100

    An updator method for bookings - this allows for routing which changes the status of a booking (accepted or rejected) - booking model now has a enum with these three states)

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,32 @@
 class BookingsController < ApplicationController
-  
+
+  # Default status of the booking needs to be pending in the create method !!!! IMPROTANT
+
+
+    def update
+      @booking = Booking.find(params[:id])
+      method = params[:action]
+      @booking.public_send(method)
+    end
+
+    # a user will sendn a "params[:action]" when they click on either the accept or the reject button
+    # The route of the button will tell us which method to call
+    # The method ALREADY EXISTS thanks to enum - those being the accepted! and rejected! methods
+    # for further reference see: https://api.rubyonrails.org/v5.1/classes/ActiveRecord/Enum.html
+
+    # accept Button /bookmars/1?action=accepted!
+    # reject Button /bookmars/1?action=rejected!
+
+    # def accept
+    #   @booking = Booking.find(params[:id])
+    #   @booking.accepted!
+    #   redirect_to @booking.trip
+    # end
+
+    # def rejected
+    #   @booking = Booking.find(params[:id])
+    #   @booking.rejected!
+    #   redirect_to @booking.trip
+    # end
+
 end
