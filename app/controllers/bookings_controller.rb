d43c9c67b0c20b5239e4d5aaee63957a5fb85f84
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2022-11-22 12:04:52 +0100

    new and create methods for bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,31 @@
 class BookingsController < ApplicationController
-  
+
+  def new
+    set_trip
+    @booking = Booking.new
+    @booking.user = current_user
+    @booking.trip = @trip
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.trip = set_trip
+    @booking.user = current_user
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+
+  def set_trip
+    @trip = Trip.find(params[:trip_id])
+  end
+
 end
