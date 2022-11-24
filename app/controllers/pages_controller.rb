746253c8430c723c9e54cae0d37efab7ec9d3266
Chris McCarthy <chmc87@gmail.com>
2022-11-24 14:32:10 +0100

    fin-mega-merge

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,23 @@
+require 'open-uri'
+require 'json'
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    image
+  end
 
+  def image
+    @results = []
+    5.times do
+      @results << serialize["photos"].sample["img_src"]
+    end
+  end
+
+  def serialize
+    url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2022-11-1&api_key=DEMO_KEY"
+    attempt_serialized = URI.open(url).read
+      JSON.parse(attempt_serialized)
   end
 end
